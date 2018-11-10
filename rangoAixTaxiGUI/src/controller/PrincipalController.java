5e55d9867c3fb0b4afeee25eb280247e45c52ff8
Eduardo <eduschadesoares@hotmail.com>
2018-11-10 17:33:08 -0200

    Last

--- rangoAixTaxiGUI/src/controller/PrincipalController.java
+++ rangoAixTaxiGUI/src/controller/PrincipalController.java
@@ -6,21 +6,31 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import utility.AeronaveFiler;
+import model.Student;
 
 public class PrincipalController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        AeronaveFiler objectIO = new AeronaveFiler();
+
+        Student student = new Student("John", "Frost", 22);
+        objectIO.WriteObjectToFile(student);
+
+        //Read object from file
+        Student st = (Student) objectIO.ReadObjectFromFile();
+        System.out.println(st);
+
+    }
+
 }
